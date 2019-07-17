271f74cd44f8997a72ec1ae693a245312c58f3ff
Piotr Krupa <piotrkrupa06@gmail.com>
2019-07-18 00:20:44 +0200

    Simplified Vertex

--- MineClone/src/Graphics/Vertex.hpp
+++ MineClone/src/Graphics/Vertex.hpp
@@ -9,44 +9,4 @@ struct Vertex
 	glm::vec3 position { 0.f };
 	glm::vec3 color { 1.f };
 	glm::vec2 texCoord { 0.f };
-
-	// constructor
-	constexpr Vertex() noexcept = default;
-	constexpr Vertex(const glm::vec3& position)
-		:
-		position(position)
-	{
-	}
-
-	constexpr Vertex(const glm::vec3& position, const glm::vec3& color)
-		:
-		position(position),
-		color(color)
-	{
-	}
-
-
-	constexpr Vertex(const glm::vec3& position, const glm::vec2& texCoord)
-		:
-		position(position),
-		texCoord(texCoord)
-	{
-	}
-
-
-	constexpr Vertex(const glm::vec3& position, const glm::vec3& color, const glm::vec2& texCoord)
-		:
-		position(position),
-		color(color),
-		texCoord(texCoord)
-	{
-	}
-
-	// copy
-	Vertex(const Vertex& other);
-	Vertex& operator=(const Vertex& other) noexcept;
-
-	// move
-	Vertex(Vertex&& other) noexcept;
-	Vertex& operator=(Vertex&& other) noexcept;
 };
